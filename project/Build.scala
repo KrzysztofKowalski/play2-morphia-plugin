55c32e3d8c26096eaf81b1292b4bac0c1b8d0245
Mike Nikles <mike@vanniksoftware.com>
2013-12-16 11:38:52 -0800

    Upgraded to Play 2.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -13,21 +13,16 @@ object Play2MorphiaPluginBuild extends Build {
     settings = buildSettings ++ Seq(
       libraryDependencies := runtime ++ test,
       publishMavenStyle := true,
-      publishTo := {
-        if (buildVersion.trim.endsWith("SNAPSHOT"))
-          Some(dropboxSnapshotRepository)
-        else
-          Some(dropboxReleaseRepository)
-      },
+      publishTo := Some(githubRepository),
       scalacOptions ++= Seq("-Xlint", "-deprecation", "-unchecked", "-encoding", "utf8"),
-      javacOptions ++= Seq("-source", "1.6", "-encoding", "utf8"),
+      javacOptions ++= Seq("-source", "1.7", "-encoding", "utf8"),
       resolvers ++= Seq(DefaultMavenRepository, Resolvers.typesafeRepository),
       checksums := Nil // To prevent proxyToys downloding fails https://github.com/leodagdag/play2-morphia-plugin/issues/11
     )
   ).settings()
 
   object Resolvers {
-    val githubRepository = Resolver.file("GitHub Repository", Path.userHome / "dev" / "leodagdag.github.com" / "repository" asFile)(Resolver.ivyStylePatterns)
+    val githubRepository = Resolver.file("GitHub Repository", Path.userHome / "Development" / "vannik-software" / "vanniksoftware.github.com" / "repository" asFile)(Resolver.ivyStylePatterns)
     val dropboxReleaseRepository = Resolver.file("Dropbox Repository", Path.userHome / "Dropbox" / "Public" / "repository" / "releases" asFile)
     val dropboxSnapshotRepository = Resolver.file("Dropbox Repository", Path.userHome / "Dropbox" / "Public" / "repository" / "snapshots" asFile)
     val typesafeRepository = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
@@ -39,18 +34,18 @@ object Play2MorphiaPluginBuild extends Build {
       "org.mongodb.morphia" % "morphia-logging-slf4j" % "0.105",
       "org.mongodb.morphia" % "morphia-validation" % "0.105",
       "org.mongodb" % "mongo-java-driver" % "2.11.0",
-      "play" %% "play-java" % "2.1.0" % "provided"
+      "com.typesafe.play" %% "play-java" % "2.2.1" % "provided"
     )
     val test = Seq(
-      "play" %% "play-test" % "2.1.0" % "test"
+      "com.typesafe.play" %% "play-test" % "2.2.1" % "provided"
     )
   }
 
   object BuildSettings {
     val buildOrganization = "leodagdag"
-    val buildVersion = "0.0.15-SNAPSHOT"
+    val buildVersion = "0.0.17"
     val buildScalaVersion = "2.10.0"
-    val buildSbtVersion = "0.12.2"
+    val buildSbtVersion = "0.13.0"
     val buildSettings = Defaults.defaultSettings ++ Seq(
       organization := buildOrganization,
       version := buildVersion,
